5148fb77cb7fb6a23b39ee1d28892a825732c0f3
pondes <salil.ponde@gmail.com>
2017-11-01 21:39:30 +0000

    Setup ricomaster and ricoagent skeleton

--- cmd/ricoagent/main.go
+++ cmd/ricoagent/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	basePath string
+	cfgFile  string
 )
 
 func main() {
-	fmt.Println("ricoagent")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.Flags().StringVar(&basePath, "path", "C:/ricotta/agent", "")
+	cfgFile = basePath + "/conf/config.yaml"
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		viper.ReadInConfig()
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Ricotta Agent")
+		agentid := viper.GetString("id")
+		fmt.Printf("Agent ID: %s\n", agentid)
+	},
 }
--- cmd/ricomaster/main.go
+++ cmd/ricomaster/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	basePath string
+	cfgFile  string
 )
 
 func main() {
-	fmt.Println("ricomaster")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.Flags().StringVar(&basePath, "path", "C:/ricotta/master", "")
+	cfgFile = basePath + "/conf/config.yaml"
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		viper.ReadInConfig()
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Ricotta Master")
+		masterid := viper.GetString("id")
+		fmt.Printf("Master ID: %s\n", masterid)
+	},
 }
